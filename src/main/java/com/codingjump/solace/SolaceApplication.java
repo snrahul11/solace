fe6b4e6a4e0cc52ae8ce057ffb201a790a98dc25
Rahul Suresh Neelakantan <snrahul11@gmail.com>
2023-02-11 13:15:11 +0530

    UPDATE: Created solace project for codingjump

---
+++ src/main/java/com/codingjump/solace/SolaceApplication.java
@@ -0,0 +1,13 @@
+package com.codingjump.solace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingjump/solace/SolaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingjump.solace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
